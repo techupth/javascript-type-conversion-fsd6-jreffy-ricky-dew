a5ad871cb7b9f8ded2dfc7b4e86918872163a31e
Jeffiros <krittiwat.c@gmail.com>
2024-05-09 14:43:15 +0700

    fix:correct value type

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,14 @@
+// ให้แก้ไข Bug ที่เกิดขึ้นจาก Code ด้านล่างนี้
+
+// ผลลัพธ์ในปัจจุบันของ Variable accountBalanceMessage เป็น "Your account balance is 40010" ซึ่งไม่ถูกต้อง
+
+// ผลลัพธ์ที่ถูกต้องของ Variable accountBalanceMessage ต้องเป็น "Your account balance is 410"
+
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-accountBalance = accountBalance + depositAmountFromUser;
+
+accountBalance = accountBalance + Number(depositAmountFromUser);
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
